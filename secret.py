519e0660bbfe6a63ab885d1f022ee526f0d7b572
Mark Ditsworth <mwditsworth@gmail.com>
2019-04-11 20:31:38 -0500

    move bot and user acct specification to secret.py

--- secret.py
+++ secret.py
@@ -11,3 +11,6 @@ api_secret_key = API SECRET KEY
 
 access_token = ACCESS TOKEN
 access_token_secret = SECRET ACCESS TOKEN
+
+bot_handle = "@twitter_bot_handle"
+username = "your screen name" # do not include the @
