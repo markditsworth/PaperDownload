6d32b2240389af3f28af08590e5371453ab556fc
Mark Ditsworth <mwditsworth@gmail.com>
2019-04-27 13:03:55 -0500

    add runtime config

--- secret.py
+++ secret.py
@@ -6,11 +6,12 @@ Created on Sun Apr  7 11:57:08 2019
 @author: markditsworth
 """
 
-api_key = API KEY 
-api_secret_key = API SECRET KEY
+api_key = "API KEY"
+api_secret_key = "API SECRET KEY"
 
-access_token = ACCESS TOKEN
-access_token_secret = SECRET ACCESS TOKEN
+access_token = "ACCESS TOKEN"
+access_token_secret = "SECRET ACCESS TOKEN"
 
 bot_handle = "@twitter_bot_handle"
 username = "your screen name" # do not include the @
+runtime = 20 * 3600 # [hours] * 3600 = [seconds]
